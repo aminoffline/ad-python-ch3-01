2f3834b4435edebc0f52b7b4d879e7136f32faea
Amin <aminoffline@yahoo.com>
2021-08-20 18:43:42 +0430

    if there is an existing database and table for our problem  this is the Solution

--- 01.py
+++ 01.py
@@ -1,21 +1,23 @@
 import mysql.connector
 
-db_name = 'first_assignment'
-user = 'Amin'
-password = 'Adv@ncePyth0n'
-Table_name = 'person'
+db_name = 'first_assignment'      #Data base name
+user = 'Amin'                     #username in mysql
+password = 'Adv@ncePyth0n'        #password
+Table_name = 'person'             #name of table with our data in it
 config ={
     'user':user,
     'password': password,
     'host':'localhost',
     'database':db_name
 }
-
 db = mysql.connector.connect(**config)
 cursor = db.cursor()
 
 sql = F"SELECT * FROM {Table_name} ORDER BY height DESC ,weight DESC "
 cursor.execute(sql)
 data = cursor
+d = open('ordered data.txt','w')
 for i,n,w,h in data:
+    d.write(f"{n} {h} {w} \n")
     print(f"{n} {h} {w} ")
+d.close()
